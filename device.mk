f6589e8ac90d3d57ff75e947f0ec0b10e9464d79
Kevin F. Haggerty <haggertk@lineageos.org>
2017-12-22 17:04:30 -0700

    ha3g: fingerprint: Bump version to 2.1
    Change-Id: Id355acaf78387917252c56476da0ff8d3828f5d0

--- device.mk
+++ device.mk
@@ -44,7 +44,7 @@ TARGET_SCREEN_WIDTH := 1600
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.0-service \
+    android.hardware.biometrics.fingerprint@2.1-service \
     fingerprint.universal5420 \
     validityService
 
